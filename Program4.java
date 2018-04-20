336b1ea04e321714c42af6d8fb9c6d4195a4c301
Ying Ying Choi <yingying_choi@hotmail.com>
2018-04-20 19:30:05 -0400

    Initial commit.

---
+++ Program4.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of class Program4 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Program4
+{
+    public static void main(String[] arc) {
+    }
+}
