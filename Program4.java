38b6fc7912da9c3e1462d88e22bc454264b8229a
ychoi1997 <yingying_choi@hotmail.com>
2018-04-20 20:17:53 -0400

    Another comment change

--- Program4.java
+++ Program4.java
@@ -11,7 +11,7 @@ public class Program4
     public static String machine, outputFormat;
     public static int states, inputs, outputs;
     /**
-     * hello
+     * This is a new comment.
      */
     public static void main(String[] args) {
         //parsing the command line inputs in the following format:
